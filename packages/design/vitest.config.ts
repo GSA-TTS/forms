96c3ae240bc61dd73a8cfdf99948c79622634e11
Daniel Naab <dan@crushingpennies.com>
2025-01-09 14:59:33 -0600

    Starting work to use Vitest Browser Mode with Storybook

--- packages/design/vitest.config.ts
+++ packages/design/vitest.config.ts
@@ -1,4 +1,8 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 import { defineConfig, mergeConfig } from 'vitest/config';
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
 
 import viteConfig from './vite.config';
 import sharedTestConfig from '../../vitest.shared';
@@ -7,9 +11,20 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     ...sharedTestConfig,
+    plugins: [
+      storybookTest({
+        configDir: path.join(
+          dirname(fileURLToPath(import.meta.url)),
+          '.storybook'
+        ),
+        storybookScript: 'storybook dev -p 9011 --no-open',
+        storybookUrl: 'http://localhost:9011',
+      }),
+      //storybookNextJsPlugin(), // 👈 Apply the framework plugin here
+    ],
     test: {
       ...sharedTestConfig.test,
-      environment: 'jsdom',
+      //environment: 'jsdom',
       setupFiles: './vitest.setup.ts',
       browser: {
         enabled: true,
@@ -17,6 +32,7 @@ export default mergeConfig(
         headless: true,
         provider: 'playwright',
       },
+      include: ['**/*.test.ts', '**/*.test.tsx', '**/*.stories.tsx'],
     },
   })
 );
