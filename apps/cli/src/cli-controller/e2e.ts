f5011253a33b2da74231304f5b484bd53dde25dd
ethangardner <ethan.gardner@gsa.gov>
2025-04-09 10:43:50 -0400

    update e2e cli to use adapted service

--- apps/cli/src/cli-controller/e2e.ts
+++ apps/cli/src/cli-controller/e2e.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import { Command } from 'commander';
 
 import { type Context } from './types.js';
-import { createAuthRepository, BaseAuthContext, createTestDbSession } from '@gsa-tts/forms-auth';
+import { createTestDbSession, createE2eAuthContext } from '@gsa-tts/forms-auth';
 
 export const addE2eCommands = (ctx: Context, cli: Command) => {
   const cmd = cli
@@ -26,46 +26,13 @@ export const addE2eCommands = (ctx: Context, cli: Command) => {
 
       try {
         console.log('Preparing database at:', dbPath);
+        const ctx = await createE2eAuthContext(dbPath);
+        const session = await createTestDbSession(ctx, 'test@example.com');
 
-        // The login flow is to create fs db context -> feed into base auth context constructor -> feed it into the auth service
-        const { createFilesystemDatabaseContext } = await import(
-          '@gsa-tts/forms-database/context'
-        );
-        const dbContext = await createFilesystemDatabaseContext(dbPath);
-        const authRepository = createAuthRepository(dbContext);
-
-        const stubLoginProvider = {};
-        const stubGetCookie = () => {};
-        const stubSetCookie = () => {};
-        const stubSetUserSession = () => {};
-        const stubIsUserAuthorized = () => Promise.resolve(true);
-        const authContext = new BaseAuthContext(
-          authRepository,
-          // Stub a login provider for testing (can be plugged in as needed)
-          // @ts-expect-error - Object literal may only specify known properties, but this is a stub.
-          stubLoginProvider,
-          stubGetCookie,
-          stubSetCookie,
-          stubSetUserSession,
-          stubIsUserAuthorized,
-        );
-
-        const email = 'test@example.com';
-
-        const user = await authRepository.createUser(email);
-        if (!user) {
-          console.log(`Test user created with id: ${email}`);
-        }
-        const userId = await authRepository.getUserId(email);
-
-        if (userId) {
-          const session = await createTestDbSession(authContext, userId);
-
-          if(session && session.id) {
-            const envContent = `AUTH_SESSION=${session.id}\nE2E_ENDPOINT=http://localhost:4321\n`;
-            await fs.writeFile(outputFile, envContent, 'utf8');
-            console.log(`.env file written to: ${outputFile}`);
-          }
+        if(session && session.id) {
+          const envContent = `AUTH_SESSION=${session.id}\nE2E_ENDPOINT=http://localhost:4321\n`;
+          await fs.writeFile(outputFile, envContent, 'utf8');
+          console.log(`.env file written to: ${outputFile}`);
         }
         console.log('Auth Context & Database Prepared Successfully!');
       } catch (error) {
